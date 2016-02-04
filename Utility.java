b1f0d427848f07e672f146a2c6a2d8b25e1542ae
imin-tom <tom@imin.co>
2016-02-04 12:25:31 +0000

    Made addToObject static

--- Utility.java
+++ Utility.java
@@ -5,7 +5,7 @@ import org.json.*;
 
 public class Utility {
 
-  public JSONObject addToObject(JSONObject object, String label, String attribute) throws JSONException {
+  public static JSONObject addToObject(JSONObject object, String label, String attribute) throws JSONException {
     try {
       object.put(label, attribute);
     } catch (JSONException e) {
