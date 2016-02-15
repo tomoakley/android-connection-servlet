bd5193ca41b553e98862ee37e5f5afa7410666c6
imin-tom <tom@imin.co>
2016-02-15 22:19:00 +0000

    Add correct imports and variables into Utility

--- Utility.java
+++ Utility.java
@@ -2,8 +2,13 @@ package ubiserv.simple.tom;
 
 import javax.json.*;
 import org.json.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utility {
+  private static Pattern pattern;
+  private static Matcher matcher;
+  private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$";
 
   public static JSONObject addToObject(JSONObject object, String label, String attribute) throws JSONException {
     try {
